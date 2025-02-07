7334487421f69aa268b7f48e8adbf9034adc4988
KOFLAzycat <lushoulei0552@163.com>
2025-02-07 17:54:12 +0800

    1. 删除部分文件

--- scenes/world/world_test.gd
+++ scenes/world/world_test.gd
@@ -3,6 +3,13 @@ class_name WorldTest extends Node2D
 @onready var boat: Boat = $Boat
 
 
+func receive_serial_data(data):
+	var parsed_data = JSON.parse(data)
+	var serialData = parsed_data.serialData
+	# 根据串口数据更新小船的位置或其他属性
+	boat.position.x += serialData.x
+	boat.position.y += serialData.y
+
 # 用键盘模拟划船动作
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("row"):
